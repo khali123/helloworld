a1a806fd9c58348c233dac6b290c6f2b5df05ec3
BIQ <khalid.biq.cod@gmail.com>
2021-02-12 05:46:51 +0100

    first commité

---
+++ Hello.java
@@ -0,0 +1,12 @@
+package projectaa;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("HELOO *************************1");
+		System.out.println("HELOO *************************2");
+
+	}
+
+}
