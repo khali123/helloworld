a08aefbde329c4c01316980ef0677c017be3300a
BIQ <khalid.biq.cod@gmail.com>
2021-02-12 06:03:22 +0100

    ren

--- Hello.java
+++ Hello.java
@@ -4,8 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 
-		System.out.println("HELOO *************************1");
-		System.out.println("HELOO *************************2");
+		System.out.println("HELLOO2 *************************1");
+		System.out.println("HELOO33 *************************2");
 
 	}
 
